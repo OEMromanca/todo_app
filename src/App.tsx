3d94f2dce3f47efcf2243d17234a486576b09dd5
romanca <r.jamrisko@gmail.com>
2023-10-31 16:14:51 +0100

    basename

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/todo_app">
         <TodoProvider>
           <div className="App">
             <Layout />

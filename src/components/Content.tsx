e0474cc82681e8ed44d2c9ede52957f92824e623
romanca <r.jamrisko@gmail.com>
2023-10-31 16:47:18 +0100

    removing routing error

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,14 +1,20 @@
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Todos from "./Todos";
 import CompletedTodos from "./CompletedTodos";
 import ActiveTodos from "./ActiveTodos";
 import SubmitTodo from "./AddTodo";
 
 const Content: React.FC = () => {
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    navigate("/todo_app");
+  }, []);
   return (
     <div className="content">
       <Routes>
-        <Route index path="/todo_app" element={<Todos />} />
+        <Route path="/todo_app" element={<Todos />} />
         <Route path="/todo_app/completed" element={<CompletedTodos />} />
         <Route path="/todo_app/active" element={<ActiveTodos />} />
         <Route path="/todo_app/submit" element={<SubmitTodo />} />

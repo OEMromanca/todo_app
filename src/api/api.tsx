b2e7e17a05407ea6188a40adb3d3b61aa3bd8476
romanca <r.jamrisko@gmail.com>
2024-02-19 14:28:29 +0100

    minor changes

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -3,61 +3,55 @@ import { ITodo } from "../interfaces/interfaces";
 
 const apiURL = "https://6532520fd80bd20280f5552c.mockapi.io/api/todos";
 
-export const getPaginatedTodosAPI = async (page: number, limit: number) => {
+export const getPaginatedTodosAPI = (page: number, limit: number) => {
   const params = new URLSearchParams();
   params.append("page", String(page));
   params.append("limit", String(limit));
 
-  return await axios.get(`${apiURL}?${params.toString()}`);
+  return axios.get(`${apiURL}?${params.toString()}`);
 };
 
-export const getPaginatedActiveTodosAPI = async (
-  page: number,
-  limit: number
-) => {
+export const getPaginatedActiveTodosAPI = (page: number, limit: number) => {
   const params = new URLSearchParams();
   params.append("page", String(page));
   params.append("limit", String(limit));
   params.append("completed", "false");
 
-  return await axios.get(`${apiURL}?${params.toString()}`);
+  return axios.get(`${apiURL}?${params.toString()}`);
 };
 
-export const getPaginatedCompletedTodosAPI = async (
-  page: number,
-  limit: number
-) => {
+export const getPaginatedCompletedTodosAPI = (page: number, limit: number) => {
   const params = new URLSearchParams();
   params.append("page", String(page));
   params.append("limit", String(limit));
   params.append("completed", "true");
 
-  return await axios.get(`${apiURL}?${params.toString()}`);
+  return axios.get(`${apiURL}?${params.toString()}`);
 };
 
-export const getAllTodosAPI = async () => {
-  return await axios.get(`${apiURL}`);
+export const getAllTodosAPI = () => {
+  return axios.get(`${apiURL}`);
 };
 
-export const addTodoAPI = async (todo: Omit<ITodo, "id">) => {
-  return await axios.post(`${apiURL}`, todo);
+export const addTodoAPI = (todo: Omit<ITodo, "id">) => {
+  return axios.post(`${apiURL}`, todo);
 };
 
-export const updateTodoAPI = async (todo: ITodo) => {
-  return await axios.put(`${apiURL}/${todo.id}`, todo);
+export const updateTodoAPI = (todo: ITodo) => {
+  return axios.put(`${apiURL}/${todo.id}`, todo);
 };
 
-export const getTodoByIdAPI = async (id: string) => {
-  return await axios.get(`${apiURL}/${id}`);
+export const getTodoByIdAPI = (id: string) => {
+  return axios.get(`${apiURL}/${id}`);
 };
 
-export const toggleCompletedTodoAPI = async (todo: ITodo) => {
+export const toggleCompletedTodoAPI = (todo: ITodo) => {
   const todoUpdate: Pick<ITodo, "completed"> = {
     completed: !todo.completed,
   };
-  return await axios.put(`${apiURL}/${todo.id}`, todoUpdate);
+  return axios.put(`${apiURL}/${todo.id}`, todoUpdate);
 };
 
-export const deleteTodoAPI = async (id: string) => {
-  return await axios.delete(`${apiURL}/${id}`);
+export const deleteTodoAPI = (id: string) => {
+  return axios.delete(`${apiURL}/${id}`);
 };
